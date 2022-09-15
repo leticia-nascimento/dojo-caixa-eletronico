e15801cc8268028a18c148c317dc9e62b69fca70
Jose Alves <jose.sousa@3778.care>
2022-09-15 16:05:24 -0300

    update README, add test

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const sack = (value) => {
-  return value;
+  return `${value}`;
 };
 
 console.log("sack", sack(42));
+
+module.exports = { sack };
--- index.test.js
+++ index.test.js
@@ -1,8 +1,10 @@
-const sack = require(".");
+const { sack } = require(".");
 
 describe("sack", () => {
   it("should sack R$20", () => {
-    const expectedValue = sack(20);
-    expect.expectedValue.toBe(20);
+    const expectedValue = sack(80);
+    expect(expectedValue).toBe(
+      "Entregar 1 nota de R$50,00 1 nota de R$ 20,00 e 1 nota de R$ 10,00."
+    );
   });
 });
