a042eaf5e176b00069a73ebf418c97b41f7dd9d4
Alexandre Hausen <alexandre.hausen@3778.care>
2022-09-19 22:21:40 -0300

    Fix expected object comparison
    - toStrictEqual instead of toBe

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const { withdraw } = require(".");
 describe("withdraw", () => {
   it("should withdraw R$80", () => {
     const expectedValue = withdraw(80);
-    expect(expectedValue).toBe({
+    expect(expectedValue).toStrictEqual({
       100: 0,
       50: 1,
       20: 1,
